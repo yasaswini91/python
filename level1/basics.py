cc1b69b404282354bcf875ca3dc4b0eab3e0929d
yasaswini91 <yasaswini1166@gmail.com>
2026-07-03 20:21:41 +0530

    patterns 6th question and mathcase statements in the basic file

--- level1/basics.py
+++ level1/basics.py
@@ -1,4 +1,10 @@
 
-#list comprehension
-x=[x*2 for x in range (1,11)]
-print(x)
+#matchcase statements
+
+def is_weekend(day):
+    match day:
+        case "saturday" | "sunday":
+            return True
+        case "monday" | "tuesday" | "wednesday" | "thursday" | "friday":
+            return False
+print(is_weekend("sunday"))
